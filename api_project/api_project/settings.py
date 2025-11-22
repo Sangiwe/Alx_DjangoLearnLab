40ed107c08494084e105d0806720dca2f93d6588
Sangiwe <nokubongasangiwe@gmail.com>
2025-11-22 13:37:53 +0200

    Completed DRF setup, serializers, API views, permissions, security configs

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     # Third-party
     'rest_framework',
-
-    # Local app
     'api',
 ]
 
