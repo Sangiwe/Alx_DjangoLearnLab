90140a412b93b005184b9b5b5ae2d9d1725ca1f4
Sangiwe <nokubongasangiwe@gmail.com>
2025-11-22 14:14:33 +0200

    Implemented authentication and permissions for API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,22 @@
+"""
+API Authentication: TokenAuthentication enabled.
+All endpoints require the user to provide a valid token.
+
+Get token:
+POST /api-token-auth/ with username + password
+"""
+
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
