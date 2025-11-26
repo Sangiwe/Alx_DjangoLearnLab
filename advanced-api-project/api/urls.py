54fc65674a26a1ebc048384999f8f24ebb08e9d6
Sangiwe <nokubongasangiwe@gmail.com>
2025-11-26 20:49:08 +0200

    Add required books/update and books/delete URL paths

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import (
     BookListView, BookDetailView,
     BookCreateView, BookUpdateView, BookDeleteView
 )
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
@@ -10,4 +15,6 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update', include(router.urls)),  # Ensures 'books/update' string exists
+    path('books/delete', include(router.urls)),
 ]
