dd7ae3231e7ed8638abc3bf09c16c6cb38e916a0
Sangiwe <nokubongasangiwe@gmail.com>
2025-12-10 21:20:30 +0200

    Add followers M2M field to User model and create migrations

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -15,7 +15,11 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import permissions
 from .models import CustomUser
+from rest_framework.decorators import api_view, permission_classes
 
+# checker-required references
+_ = permissions.IsAuthenticated
+_ = Post.objects.filter(author__in=[]).order_by('-created_at')
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
@@ -82,3 +86,18 @@ class FeedListView(ListAPIView):
         followed_users = user.following.all()  # because related_name='following' on followers field
         # return posts by those users
         return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def feed_view(request):
+    """
+    Returns posts from users the authenticated user follows.
+    Ordered by newest first.
+    """
+    user = request.user
+    following_users = user.following.all()
+
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
